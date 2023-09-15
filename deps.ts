00b447b3a651818f7b887c363ec311025491e051
Nicholas Berlette <nick@berlette.com>
2023-09-15 14:44:57 -0700

    deps: remove unused dependencies

--- deps.ts
+++ deps.ts
@@ -1,13 +1,12 @@
 export * from "https://deno.land/std@0.201.0/assert/mod.ts";
-export { assert } from "https://deno.land/std@0.201.0/assert/mod.ts";
-export { readAll } from "https://deno.land/std@0.201.0/streams/read_all.ts";
-export { writeAll } from "https://deno.land/std@0.201.0/streams/write_all.ts";
+export {
+  readAll,
+  readerFromStreamReader,
+  writeAll,
+} from "https://deno.land/std@0.201.0/streams/mod.ts";
+export { Sha256 } from "https://deno.land/std@0.160.0/hash/sha256.ts";
 export {
   inspect,
   type InspectOptions,
   type InspectOptionsStylized,
 } from "node:util";
-export {
-  readerFromStreamReader,
-} from "https://deno.land/std@0.201.0/streams/reader_from_stream_reader.ts";
-export { Sha256 } from "https://deno.land/std@0.160.0/hash/sha256.ts";
