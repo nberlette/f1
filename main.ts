7a622e8f7ff367680ea0afa8b25d66801859cfd3
Nicholas Berlette <nick@berlette.com>
2023-09-15 14:43:59 -0700

    feat: tighten permissions on main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-#!/usr/bin/env -S deno run --allow-all --unstable
+#!/usr/bin/env -S deno run --unstable -q --allow-read=assets --allow-write=assets --allow-net=deno.land,oxblue.com --allow-env=DENO_KV_PATH,GITHUB_OUTPUT,GITHUB_ENV,DEBUG,BASEDIR
 
 if (import.meta.main) {
-  await import("./src/scrape.ts").then(({ scrape }) => scrape());
+  await import("./src/scrape.ts").then((m) => m.scrape());
 }
