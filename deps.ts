b76dd760807316ff718ab0e271de2c2fcaffe8c7
Nicholas Berlette <nick@berlette.com>
2023-09-19 01:40:40 -0700

    perf: remove unused exports from deps.ts
    Signed-off-by: Nicholas Berlette <nick@berlette.com>

--- deps.ts
+++ deps.ts
@@ -1,12 +1,9 @@
-export * from "https://deno.land/std@0.201.0/assert/mod.ts";
+export { assert } from "https://deno.land/std@0.201.0/assert/assert.ts";
+export { readAll } from "https://deno.land/std@0.201.0/streams/read_all.ts";
 export {
-  readAll,
   readerFromStreamReader,
-  writeAll,
-} from "https://deno.land/std@0.201.0/streams/mod.ts";
+} from "https://deno.land/std@0.201.0/streams/reader_from_stream_reader.ts";
+export { writeAll } from "https://deno.land/std@0.201.0/streams/write_all.ts";
 export { Sha256 } from "https://deno.land/std@0.160.0/hash/sha256.ts";
-export {
-  inspect,
-  type InspectOptions,
-  type InspectOptionsStylized,
-} from "node:util";
+export { inspect } from "node:util";
+export type { InspectOptions, InspectOptionsStylized } from "node:util";
